c0bd65f9b90374d4444f652e8c3ce12f433a4ae1
Vlatko Dimitrovski <vdimitrovski@brandwatch.com>
2020-10-24 09:28:52 +0200

    init repo

---
+++ src/main/java/com/vlatko/autoplac/AutoPlacApplication.java
@@ -0,0 +1,13 @@
+package com.vlatko.autoplac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPlacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPlacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlatko/autoplac/AutoPlacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlatko.autoplac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPlacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
